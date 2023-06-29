32c8fb358c8ff84135e9c9cbb88619d0ca6b5ba1
Eyal Chojnowski <eyal.choj@gmail.com>
2023-06-29 16:35:41 +0200

    :zap: Switch ChaCha20 for HMAC-DRBG

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ it in a node.js environment. The code in `test` uses the WebCrypto library so it
 compatible with any modern browser environment.
 
 The directory `arsnap-keygen` is a Rust library containing a single function `keygen`
-that generate an Arweave key. It takes as an argument an array of 32 bytes representing the seed
+that generates an Arweave key. It takes as an argument an array of 32 bytes representing the seed
 that will be used to generate the key in a deterministic manner.
 
 A simple npm package is also included in the directory `test` that illustrate how such a library
--- arsnap-keygen/src/lib.rs
+++ arsnap-keygen/src/lib.rs
@@ -1,13 +1,48 @@
 use wasm_bindgen::prelude::*;
 
-use rand::SeedableRng;
-use rand_chacha::ChaCha20Rng;
+use hmac_drbg::HmacDRBG;
+use rand::RngCore;
 use rsa::{pkcs8::ToPrivateKey, RsaPrivateKey};
+use sha2::Sha256;
+
+struct Prng {
+    drbg: HmacDRBG<Sha256>,
+}
+
+impl Prng {
+    fn new(seed: &[u8]) -> Self {
+        let drbg = HmacDRBG::<Sha256>::new(seed, &[], &[]);
+
+        Self { drbg }
+    }
+}
+
+impl RngCore for Prng {
+    fn next_u32(&mut self) -> u32 {
+        let mut output = [0u8; 4];
+        self.drbg.generate_to_slice(&mut output, None);
+        u32::from_ne_bytes(output)
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        let mut output = [0u8; 8];
+        self.drbg.generate_to_slice(&mut output, None);
+        u64::from_ne_bytes(output)
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        self.drbg.generate_to_slice(dest, None);
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+        Ok(self.fill_bytes(dest))
+    }
+}
 
 #[wasm_bindgen]
 pub fn keygen(secret: &[u8]) -> String {
     if secret.len() != 32 {
-        panic!("Secret must be at least 32 bytes long");
+        panic!("Secret must be 32 bytes long");
     }
 
     let size: usize = 4096;
@@ -15,7 +50,7 @@ pub fn keygen(secret: &[u8]) -> String {
     let mut seed: [u8; 32] = [0u8; 32];
     seed.clone_from_slice(&secret[0..32]);
 
-    let mut rng = ChaCha20Rng::from_seed(seed);
+    let mut rng = Prng::new(&seed);
 
     let key = RsaPrivateKey::new(&mut rng, size).unwrap();
     let pem = key.to_pkcs8_der().unwrap().to_pem().to_string();
